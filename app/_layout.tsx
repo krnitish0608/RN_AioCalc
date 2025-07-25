bfe21a420bdc35fae0c03b082a72aae06e67b67d
krnitish0608 <krnitish0608@gmail.com>
2025-07-25 15:56:25 +0530

    fff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
+    // Async font loading only occurs in development.llll
     return null;
   }
 
